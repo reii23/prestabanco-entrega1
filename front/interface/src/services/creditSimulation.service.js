487f37619027cfbd2121b27fa04fbcaa1a843ef1
reii23 <reinaldo.pacheco@usach.cl>
2024-11-02 18:13:05 -0300

    add http to services and change ports in docker-compose

--- front/interface/src/services/creditSimulation.service.js
+++ front/interface/src/services/creditSimulation.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "prestabanco-app.brazilsouth.cloudapp.azure.com:80/api/CreditSimulation/";
+const API_URL = "http://prestabanco-app.brazilsouth.cloudapp.azure.com:80/api/CreditSimulation/";
 
 const simulateCredit = (creditSimulation) => {
     return axios.post(API_URL, creditSimulation);
