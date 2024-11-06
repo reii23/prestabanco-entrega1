67735dc6856a5fb2cb1fc68b9e4d1d5d8aea5157
reii23 <reinaldo.pacheco@usach.cl>
2024-11-06 14:06:01 -0300

    prueba de actualizacion jenkins

--- front/interface/src/components/Home.jsx
+++ front/interface/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>PrestaBanco</h1>
+            <h1>PrestaBanco App</h1>
             <p>Este sistema le permite gestionar clientes, solicitudes de crédito, y otros aspectos relacionados con los préstamos hipotecarios</p>
             <p>Use el menú lateral para navegar entre las diferentes secciones.</p>
         </div>
